06b824c4c357097e4fe44456e1c242e49e302789
Ofir <Ofir@localhost>
2012-06-29 01:27:25 +0300

    allow save

---
+++ main.py
@@ -0,0 +1,81 @@
+from BaseHTTPServer import HTTPServer
+from CGIHTTPServer import CGIHTTPRequestHandler
+import pickle
+#static_page = open("index.html", "rt").read()
+#jquery_page = open("jquery.min.js", "rt").read()
+lines = []
+lines_index = 0
+    
+class CompGeoRequestHandler(CGIHTTPRequestHandler):
+    def myheaders(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+    def do_GET(self):
+        global lines_index
+        global lines
+        #global static_page
+
+        try:
+            ind = int(self.path[1:])
+            if ind - lines_index < 0: 
+                lines_index = 0
+                ind = 0
+            self.myheaders()
+            self.wfile.write(repr([lines[ind - lines_index:], lines_index, len(lines)]).replace("'", '"'))
+        except ValueError:
+            if self.path.startswith('/add'):
+                coords = [a for a in self.path[len('/add/'):].split('/')]
+                lines.append(coords)
+                self.myheaders()
+                self.wfile.write("")
+            elif self.path.startswith('/clean'):
+                lines_index += len(lines)
+                lines = []
+                self.myheaders()
+                self.wfile.write("")
+            elif self.path.startswith('/save'):
+                pickle.dump(lines, open('objects.pkl', 'wb'))
+                self.myheaders()
+                self.wfile.write("")
+            elif self.path.startswith('/undo'):
+                #lines_index -= 1
+                del lines[len(lines) - 1]
+                self.myheaders()
+                self.wfile.write("")
+            elif self.path == '/':
+                #self.wfile.write(static_page)
+                self.myheaders()
+                self.wfile.write(open("index.html", "rt").read())
+            elif self.path == '/jquery.min.js':
+                #self.wfile.write(jquery_page)
+                self.send_response(200)
+                self.send_header('Content-type', 'application/javascript')
+                self.end_headers()
+                self.wfile.write(open("jquery.min.js", "rt").read())
+
+        
+
+def start_server(port):
+    global lines
+    try:
+        lines = pickle.load(open('objects.pkl', "rb"))
+    except:
+        lines = []
+        
+    server = HTTPServer(('', port), CompGeoRequestHandler)
+    try:
+        server.serve_forever()
+    except:
+        pickle.dump(lines, open('objects.pkl', 'wb'))
+
+
+
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) > 1:
+        port = int(sys.argv[1])
+    else:
+        port = 32156
+    start_server(port)
